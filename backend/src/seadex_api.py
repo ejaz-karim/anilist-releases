974df10f10ec252d9d1f225606ce601da3e94d9f
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-05-18 21:34:23 +0100

    Fetch more variables from API

--- backend/src/seadex_api.py
+++ backend/src/seadex_api.py
@@ -1,5 +1,5 @@
 import requests
-import json
+
 
 class SeadexApi:
     def get_release_data(self, anilist_id):
@@ -8,28 +8,27 @@ class SeadexApi:
         response = requests.get(trs_url)
         response.raise_for_status()
 
-        
         data = response.json()
-        items = data["items"][0]
-        comparison = items["comparison"]
-        notes = items["notes"]
-        theoretical_best = items["theoreticalBest"]
+        items = data.get["items"][0]
 
-        
+        comparison = items.get("comparison")
+        notes = items.get("notes")
+        theoretical_best = items.get("theoreticalBest")
+
         trs = items["expand"]["trs"]
 
         for entry in trs:
             tracker = entry.get("tracker")
             release_group = entry.get("releaseGroup")
             url = entry.get("url")
+            dual_audio = entry.get("dualAudio")
+            is_best = entry.get("isBest")
 
             files = entry.get("files")
 
             for file in files:
                 name = file.get("name")
                 file_size = self.format_file_size(file.get("length"))
-
-
 
             if entry.get("infoHash") == "<redacted>":
                 private_tracker = True
@@ -38,14 +37,9 @@ class SeadexApi:
 
 
 
-
-
-        # release = data["items"][0]["expand"]["trs"][0]["releaseGroup"]
-       
-
     def format_file_size(self, bytes):
-        megabytes = bytes / (1024 ** 2)
-        gigabytes = bytes / (1024 ** 3)
+        megabytes = bytes / (1024**2)
+        gigabytes = bytes / (1024**3)
 
         if gigabytes >= 1:
             return f"{round(gigabytes, 1)}" + " GiB"
@@ -53,10 +47,5 @@ class SeadexApi:
             return f"{round(megabytes, 1)}" + " MiB"
 
 
-
-
 if __name__ == "__main__":
-    # print(SeadexApi().get_release_data(18897))
     SeadexApi().get_release_data(18897)
-
-
