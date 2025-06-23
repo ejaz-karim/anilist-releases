9d5a1bdeb179fb1bce94b435c6f710f68f7a0109
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-06-23 17:15:21 +0100

    Define get_anidb_id

--- backend/src/anidb_id_api.py
+++ backend/src/anidb_id_api.py
@@ -1,4 +1,10 @@
 import requests
 
+
 class AnidbIdApi:
-    pass
+    def get_anidb_id(self, anilist_url):
+        base_url = "https://find-my-anime.dtimur.de/api?id=9253&provider=Anilist&includeAdult=true&collectionConsent=false"
+
+
+if __name__ == "__main__":
+    print(AnidbIdApi().get_anidb_id("https://anilist.co/anime/9253/SteinsGate/"))
