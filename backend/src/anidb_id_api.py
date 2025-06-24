f6d0e7a62fdfcfe892a708606fb57e241c7e5893
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-06-24 23:51:40 +0100

    Define get_anilist_id

--- backend/src/anidb_id_api.py
+++ backend/src/anidb_id_api.py
@@ -5,6 +5,14 @@ class AnidbIdApi:
     def get_anidb_id(self, anilist_url):
         base_url = "https://find-my-anime.dtimur.de/api?id=9253&provider=Anilist&includeAdult=true&collectionConsent=false"
 
+        base_url2 = "https://api.ani.zip/v1/mappings?anilist_id=16498"
+
+        base_url3 = "https://zenshin-supabase-api.onrender.com/mappings?anilist_id=16498"
+
+    def get_anilist_id(self, anilist_url):
+        pass
+
+
 
 if __name__ == "__main__":
     print(AnidbIdApi().get_anidb_id("https://anilist.co/anime/9253/SteinsGate/"))
