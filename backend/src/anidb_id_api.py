c9b2ca42e85a7a79b81af6988b5b25129b561321
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-07-22 04:11:55 +0100

    Add try-except to get_anidb_id

--- backend/src/anidb_id_api.py
+++ backend/src/anidb_id_api.py
@@ -3,23 +3,37 @@ import requests
 
 class AnidbIdApi:
     def get_anidb_id(self, anilist_id):
-        find_my_anime_api_url = f"https://find-my-anime.dtimur.de/api?id={anilist_id}&provider=Anilist&includeAdult=true&collectionConsent=false"
-        fma_response = requests.get(find_my_anime_api_url)
-        fma_response.raise_for_status()
-        fma_data = fma_response.json()
-        fma_anidb_id = fma_data[0].get("providerMapping").get("AniDB")
+        try:
+            find_my_anime_api_url = f"https://find-my-anime.dtimur.de/api?id={anilist_id}&provider=Anilist&includeAdult=true&collectionConsent=false"
+            fma_response = requests.get(find_my_anime_api_url)
+            fma_response.raise_for_status()
+            fma_data = fma_response.json()
+            fma_anidb_id = fma_data[0].get("providerMapping").get("AniDB")
+            return fma_anidb_id
+        except (requests.RequestException, KeyError, IndexError, TypeError):
+            pass
 
-        api_ani_url = f"https://api.ani.zip/v1/mappings?anilist_id={anilist_id}"
-        api_ani_response = requests.get(api_ani_url)
-        api_ani_response.raise_for_status()
-        api_ani_data = api_ani_response.json()
-        api_ani_anidb_id = api_ani_data.get("anidb_id")
+        try:
+            api_ani_url = f"https://api.ani.zip/v1/mappings?anilist_id={anilist_id}"
+            api_ani_response = requests.get(api_ani_url)
+            api_ani_response.raise_for_status()
+            api_ani_data = api_ani_response.json()
+            api_ani_anidb_id = api_ani_data.get("anidb_id")
+            return api_ani_anidb_id
+        except (requests.RequestException, ValueError, KeyError, TypeError):
+            pass
 
-        zenshin_api_url = f"https://zenshin-supabase-api.onrender.com/mappings?anilist_id={anilist_id}"
-        zenshin_response = requests.get(zenshin_api_url)
-        zenshin_response.raise_for_status()
-        zenshin_data = zenshin_response.json()
-        zenshin_anidb_id = zenshin_data.get("mappings").get("anidb_id")
+        try:
+            zenshin_api_url = f"https://zenshin-supabase-api.onrender.com/mappings?anilist_id={anilist_id}"
+            zenshin_response = requests.get(zenshin_api_url)
+            zenshin_response.raise_for_status()
+            zenshin_data = zenshin_response.json()
+            zenshin_anidb_id = zenshin_data.get("mappings").get("anidb_id")
+            return zenshin_anidb_id
+        except (requests.RequestException, ValueError, KeyError, TypeError):
+            pass
+
+        return None
 
     def get_anilist_id(self, anilist_url):
         if "://" in anilist_url:
